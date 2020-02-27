7781863c3d9a7ef4dae3af9654a745341c60d0b5
YoussefNabet <s112695@ap.be>
2020-02-27 17:21:30 +0100

    EF Core, EF Core Tools and EF Core SqlServer nugget packages added

---
+++ Models/EFEmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BouwJeBlutNV.Models
+{
+    public class EFEmployeeRepository : IEmployeeRepository
+    {
+        public Employee Create(Employee employeeToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Delete(Employee employeeToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BouwJeBlutNV.Models
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployees();
+
+        Employee GetEmployeeById(int id);
+        Employee Create(Employee employeeToCreate);
+        Employee Delete(Employee employeeToDelete);
+        Employee Update(Employee employeeChanges);
+    }
+}
